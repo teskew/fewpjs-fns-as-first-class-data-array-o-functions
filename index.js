7c61a001d625c027a61006d4987fbf0b21f4a9c7
github username <github email address>
2021-09-05 16:00:06 -0500

    commit tree e88cf94455d23f095cdfd9519ffa2300e6d661da

---
+++ index.js
@@ -0,0 +1,24 @@
+
+  function wakeDog(dogName, dogBreed) {
+    return(`Wake ${dogName} the ${dogBreed}`);
+  }
+  function leashDog(dogName, dogBreed) {
+    return(`Leash ${dogName} the ${dogBreed}`);
+  }
+  function walkToPark(dogName, dogBreed) {
+   return(`Walk to the park with ${dogName} the ${dogBreed}`);
+  }
+  function throwFrisbee(dogName, dogBreed) {
+   return(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  }
+  function walkHome(dogName, dogBreed) {
+    return(`Walk home with ${dogName} the ${dogBreed}`);
+  }
+ 
+  function unleashDog(dogName, dogBreed) {
+   return(`Unleash ${dogName} the ${dogBreed}`);
+  }
+  var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+ function exerciseDog(dog, breed){
+     return routine.map(fn => fn(dog, breed))
+ }
